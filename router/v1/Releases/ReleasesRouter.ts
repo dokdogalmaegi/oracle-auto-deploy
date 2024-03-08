0df72acc69bd2b46b4fb4d44d0285a13ef9b9c93
dokdogalmaegi <dokdogalmaegi@gmail.com>
2024-03-08 11:34:51 +0900

    폴더 이름 변경

--- router/v1/Releases/ReleasesRouter.ts
+++ router/v1/Releases/ReleasesRouter.ts
@@ -1,15 +1,12 @@
 import { Router, Request, Response } from "express";
-import { GoogleSheet } from "../../../service/GoogleSheet/GoogleDataAccessService";
+import { GoogleSheet } from "../../../service/googleSheet/GoogleDataAccessService";
 import path from "path";
 import fs from "fs";
 
 const releasesV1Router: Router = Router();
 
 releasesV1Router.get("/", async (req: Request, res: Response) => {
-  const sheetInfoUrl = path.resolve(
-    __dirname,
-    "../../../config/sheetInfo.json"
-  );
+  const sheetInfoUrl = path.resolve(__dirname, "../../../config/sheetInfo.json");
   const sheetInfo = JSON.parse(fs.readFileSync(sheetInfoUrl, "utf8"));
 
   const googleSheet = new GoogleSheet(sheetInfo.testSpreadSheetId);
