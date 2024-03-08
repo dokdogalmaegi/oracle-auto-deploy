4977b3ba8f9fff1a8f1fe83865d0b306d0c9a259
dokdogalmaegi <dokdogalmaegi@gmail.com>
2024-03-08 16:26:52 +0900

    env 추가

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import express, { Express } from "express";
 import ApiV1Router from "./router/MainRouter";
-import fs from "fs";
+import dotenv from "dotenv";
 import path from "path";
 
 const app: Express = express();
@@ -15,9 +15,6 @@ app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
 
-const sheetInfoUrl = path.resolve(__dirname, "config/sheetInfo.json");
-const sheetInfo = JSON.parse(fs.readFileSync(sheetInfoUrl, "utf8"));
-
-process.env.TEST_SPREAD_SHEET_ID = sheetInfo.testSpreadSheetId;
+dotenv.config({ path: path.resolve(__dirname, "./config/.env") });
 
 export default app;
--- router/v1/releases/ReleasesRouter.ts
+++ router/v1/releases/ReleasesRouter.ts
@@ -5,7 +5,7 @@ import { getRows } from "../../../service/release/ReleaseService";
 const releasesV1Router: Router = Router();
 
 releasesV1Router.get("/", async (req: Request, res: Response) => {
-  const googleSheet = new GoogleSheet(process.env.TEST_SPREAD_SHEET_ID!);
+  const googleSheet = new GoogleSheet(process.env.SHEET_ID!);
   const headerColumns = await googleSheet.getHeaderColumnFromTwoRows();
   const rows = await getRows(headerColumns);
 
