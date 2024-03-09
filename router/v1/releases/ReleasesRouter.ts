89d2e30e17e615764920d27e0c59737ecf230666
dokdogalmaegi <clik202012@gmail.com>
2024-03-09 20:19:23 +0900

    refac 및 중복 row 제거

--- router/v1/releases/ReleasesRouter.ts
+++ router/v1/releases/ReleasesRouter.ts
@@ -1,9 +1,8 @@
 import { Router, Request, Response, NextFunction } from "express";
 import { GoogleSheet } from "../../../service/googleSheet/GoogleDataAccessService";
-import { getRowsWithHeaderColumn, isReleaseTargetRow } from "../../../service/release/ReleaseService";
+import { getReleaseTargetList, getRowsWithHeaderColumn } from "../../../service/release/ReleaseService";
 import { SuccessResponseData } from "../../../utils/ResponseUtils";
 import { asyncRouter } from "../../../utils/RouterUtils";
-import { ReleaseTarget } from "../../../model/releases/ReleaseTarget";
 
 const releasesV1Router: Router = Router();
 
@@ -11,17 +10,11 @@ releasesV1Router.get(
   "/",
   asyncRouter(async (_: Request, res: Response, next: NextFunction) => {
     const googleSheet = new GoogleSheet(process.env.SHEET_ID!);
+
     const headerColumns = await googleSheet.getHeaderColumnFromTwoRows();
     const rows = await getRowsWithHeaderColumn(headerColumns);
 
-    const releaseTargetList = rows?.filter(isReleaseTargetRow).map((row) => {
-      return new ReleaseTarget(
-        row.getCellFilteredByHeaderLabel("OBJECT NAME")!.value.trim(),
-        row.getCellFilteredByHeaderLabel("MODIFIED_S")!.value.length > 0,
-        row.getCellFilteredByHeaderLabel("MODIFIED_B")!.value.length > 0
-      );
-    });
-    const response = new SuccessResponseData("Success to get release data", releaseTargetList || []);
+    const response = new SuccessResponseData("Success to get release target data", getReleaseTargetList(rows));
     return res.json(response.json);
   })
 );
--- service/release/ReleaseService.ts
+++ service/release/ReleaseService.ts
@@ -2,33 +2,38 @@ import { GoogleSheet } from "../googleSheet/GoogleDataAccessService";
 import { Cell } from "../../model/googleSheet/Cell";
 import { Row } from "../../model/googleSheet/Row";
 import { HeaderColumn } from "../../model/googleSheet/HeaderColumn";
+import { ReleaseTarget } from "../../model/releases/ReleaseTarget";
 
 export const getRowsWithHeaderColumn = async (
   headerColumns: HeaderColumn[],
   sheetName: string = ""
-): Promise<Row[] | undefined> => {
+): Promise<Row[]> => {
   const firstColumn = headerColumns[0].column;
   const lastColumn = headerColumns[headerColumns.length - 1].column;
 
   const googleSheet = new GoogleSheet(process.env.SHEET_ID!);
   const rowDataList = await googleSheet.getValuesOf(`${firstColumn}3`, lastColumn, sheetName);
 
-  return rowDataList?.map((cellDataList, rowIdx) => {
-    const rowNumber = rowIdx + 3;
+  if (!rowDataList) {
+    return [];
+  } else {
+    return rowDataList.map((cellDataList, rowIdx) => {
+      const rowNumber = rowIdx + 3;
 
-    if (cellDataList.length > headerColumns.length) {
-      throw new Error(`Row ${rowNumber} has more cells than header columns`);
-    }
+      if (cellDataList.length > headerColumns.length) {
+        throw new Error(`Row ${rowNumber} has more cells than header columns`);
+      }
 
-    const cells = cellDataList.map((value, idx) => {
-      return new Cell(headerColumns[idx], rowNumber, value);
+      const cells = cellDataList.map((value, idx) => {
+        return new Cell(headerColumns[idx], rowNumber, value);
+      });
+
+      return new Row(cells, rowNumber);
     });
-
-    return new Row(cells, rowNumber);
-  });
+  }
 };
 
-export const isReleaseTargetRow = (row: Row): boolean => {
+const isReleaseTargetRow = (row: Row): boolean => {
   const modifyTable = row.existsCellValueByHeaderLabel("MODIFIED_T");
   const modifyData = row.existsCellValueByHeaderLabel("MODIFIED_D");
   if (modifyTable || modifyData) {
@@ -40,4 +45,26 @@ export const isReleaseTargetRow = (row: Row): boolean => {
   const modifyBody = row.existsCellValueByHeaderLabel("MODIFIED_B");
 
   return status === "R" && (modifySpec || modifyBody) ? true : false;
+};
+
+export const getReleaseTargetList = (rows: Row[]): ReleaseTarget[] => {
+  const releaseList = rows.filter(isReleaseTargetRow).map((row) => {
+    return new ReleaseTarget(
+      row.getCellFilteredByHeaderLabel("OBJECT NAME")!.value.trim(),
+      row.getCellFilteredByHeaderLabel("MODIFIED_S")!.value.length > 0,
+      row.getCellFilteredByHeaderLabel("MODIFIED_B")!.value.length > 0
+    );
+  });
+
+  const releaseTargetNames = [...new Set(releaseList.map((release) => release.packageName))];
+
+  return releaseTargetNames.map((packageName) => {
+    const packageItems = releaseList.filter((release) => release.packageName === packageName);
+
+    return new ReleaseTarget(
+      packageName,
+      packageItems.some((item) => item.isModifySpec),
+      packageItems.some((item) => item.isModifyBody)
+    );
+  });
 };
