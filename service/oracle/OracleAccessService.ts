2422abae69670a875d691e6d4b609f5ca5f87b70
dokdogalmaegi <clik202012@gmail.com>
2024-03-10 22:46:25 +0900

    Refac OracleAccessService

--- service/oracle/OracleAccessService.ts
+++ service/oracle/OracleAccessService.ts
@@ -9,25 +9,39 @@ const getConnection = async (server: string) => {
   return await OracleDB.getConnection(serverConnection.connectionInfo);
 };
 
-export const compileSql = async (sql: string, server: string) => {
-  const connection = await getConnection(server);
-
-  logger.info(`Try to compile SQL on ${server} server`);
-  await connection.execute(sql);
-  await connection.close();
-};
-
-export const executeSqlList = async (sqlList: string[], connection: OracleDB.Connection, tryCount: number = 0) => {
+const executeSqlList = async (
+  sqlList: string[],
+  connection: OracleDB.Connection,
+  tryCount: number = 0,
+  tryMaxCount: number = 5
+) => {
+  const errorSqlList: string[] = [];
+  let currentSql: string = "";
   try {
     for (const sql of sqlList) {
+      currentSql = sql;
       await connection.execute(sql);
     }
   } catch (error) {
-    if (tryCount < 3) {
-      logger.warn(`Failed to execute SQL on ${connection.oracleServerVersion}, try again`);
-      await executeSqlList(sqlList, connection, tryCount + 1);
-    } else {
-      throw new Error(`Failed to execute SQL on ${connection.oracleServerVersion}`);
-    }
+    logger.error(`Error: ${error}`);
+    errorSqlList.push(currentSql);
   }
+
+  if (errorSqlList.length > 0 && tryCount < tryMaxCount) {
+    logger.warn(`Retry executing SQL list on ${errorSqlList.length} SQLs on ${connection.oracleServerVersion} server.`);
+    await executeSqlList(errorSqlList, connection, tryCount + 1);
+  } else if (errorSqlList.length > 0 && tryCount >= tryMaxCount) {
+    logger.error(`Failed to execute SQL on ${connection.oracleServerVersion} after ${tryMaxCount} times, skip it`);
+  } else {
+    logger.info(`Success to execute SQL list on ${connection.oracleServerVersion} server`);
+    logger.info(`SQL count is ${sqlList.length}`);
+  }
+};
+
+export const executeSqlListFrom = async (server: string, sqlList: string[]) => {
+  const connection = await getConnection(server);
+
+  logger.info(`Try to compile SQL on ${server} server`);
+  await executeSqlList(sqlList, connection);
+  await connection.close();
 };
