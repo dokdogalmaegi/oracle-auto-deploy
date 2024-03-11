e6b3bd7effa28c839280cad1348e03137645571b
dokdogalmaegi <dokdogalmaegi@gmail.com>
2024-03-11 11:23:03 +0900

    에러 메시지 전달하도록 변경

--- middleware/ErrorHandler.ts
+++ middleware/ErrorHandler.ts
@@ -2,6 +2,6 @@ import { NextFunction, Request, Response } from "express";
 import { FailResponseData } from "../utils/ResponseUtils";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  const failResponse = new FailResponseData("Internal Server Error", err);
+  const failResponse = new FailResponseData(err.message, err);
   return res.status(500).json(failResponse.json);
 };
