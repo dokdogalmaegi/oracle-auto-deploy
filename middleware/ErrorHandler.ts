4cd651d4c85166d6b66456ceab101a9022a6ba7c
dokdogalmaegi <dokdogalmaegi@gmail.com>
2024-03-22 09:10:14 +0900

    에러 발생 시 재시작하도록 pm2와 같이 실행하도록 가이드 필요

--- middleware/ErrorHandler.ts
+++ middleware/ErrorHandler.ts
@@ -2,9 +2,12 @@ import { NextFunction, Request, Response } from "express";
 import { FailResponseData } from "../utils/ResponseUtils";
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  const failResponse = new FailResponseData(err.message, err);
-  if (err.message === "ORA-12154: TNS:could not resolve the connect identifier specified") {
-    throw err;
+  if (err.message.includes("ORA-12154") || err.message.includes("NJS-118")) {
+    const fialeResponse = new FailResponseData("다시 시도해주세요", new Error("Thick mode change error"))
+    res.status(500).json(fialeResponse.json)
+    process.exit()
+  } else {
+    const failResponse = new FailResponseData(err.message, err);
+    return res.status(500).json(failResponse.json);
   }
-  return res.status(500).json(failResponse.json);
 };
